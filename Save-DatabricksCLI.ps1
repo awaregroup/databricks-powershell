f111cddb4bba974665ed22a90a3745ba93a954f7
Nathan Holland <6608414+NathanNZ@users.noreply.github.com>
2023-11-09 13:09:50 +1300

    Create Save-DatabricksCLI.ps1

---
+++ Save-DatabricksCLI.ps1
@@ -0,0 +1,44 @@
+#!/usr/bin/env pwsh
+param([switch]$RegisterPathInCurrentSession)
+$ErrorActionPreference = "Stop"
+
+
+$VERSION = "0.203.1" # Bump this version for newer versions
+$FILE = "databricks_cli_$VERSION"
+$TARGET = "$PSScriptRoot/databricks-cli"
+
+# Include operating system in file name.
+if (($Env:OS -eq "Windows_NT") -or $IsWindows ) {
+    $FILE = "${FILE}_windows"
+}
+elseif ($IsLinux) {
+    $FILE = "${FILE}_linux"
+}
+elseif ($IsMacOS) {
+    $FILE = "${FILE}_darwin"
+}
+
+if ($FILE -eq "databricks_cli_$VERSION") {
+    Write-Warning "Unknown operating system: $Env:OS"
+    exit 1
+}
+
+$FILE = "${FILE}_amd64"
+
+New-Item -ItemType Directory -Force -Path $TARGET
+
+# Download release archive.
+Invoke-WebRequest -Uri "https://github.com/databricks/cli/releases/download/v${VERSION}/${FILE}.zip" -OutFile "${FILE}.zip"
+
+# Unzip release archive.
+Expand-Archive -Path "${FILE}.zip" -DestinationPath $TARGET -Force
+
+Remove-Item "${FILE}.zip"
+
+if($IsLinux -or $IsMacOS){
+    & chmod +x "$TARGET/databricks"
+}
+
+if($RegisterPathInCurrentSession) {
+    $Env:Path += [IO.Path]::PathSeparator + $TARGET
+}
